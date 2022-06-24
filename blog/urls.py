d924f6fe9a10571eeb5797841a801373e1cc6e4f
HenryDani <lagunasdaniel001@gmail.com>
2022-06-23 22:17:19 -0500

    Terminando el crud

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogCreateView, BlogDeleteView, BlogListView, BlogDetailView, BlogUpdateView
 app_name = 'blog'
 urlpatterns = [
-   path('',BlogListView.as_view(), name='blog_list')
+   path('list/',BlogListView.as_view(), name='blog_list'),
+   path('create/',BlogCreateView.as_view(), name='blog_create'),
+   path('<int:pk>/detail',BlogDetailView.as_view(), name='blog_detail'),
+   path('<int:pk>/update/',BlogUpdateView.as_view(), name='blog_update'),
+   path('<int:pk>/delete/',BlogDeleteView.as_view(), name='blog_delete'),
 ]
