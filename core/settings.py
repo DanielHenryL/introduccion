df9236e7b29b759a6dd4eadda867012fa1f5ca03
HenryDani <lagunasdaniel001@gmail.com>
2022-06-24 00:43:08 -0500

    añadiendo tailwind a django

--- core/settings.py
+++ core/settings.py
@@ -26,8 +26,16 @@ INSTALLED_APPS = [
 
     'core',
     'blog',
+    'tailwind',
+    'theme',
+    'django_browser_reload'
 ]
 
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -36,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'core.urls'
